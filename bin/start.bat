ebae4596c85f9d0746678cc9239852b64d7ec929
berrycoiled <berrycoiled@users.noreply.github.com>
2017-07-17 21:51:01 -0600

    Update Windows start script for web interface. Switches to the batch file multiline command syntax ^. Provides behavior and output consistent with Linux start script. Maintains the issue of leaking the web interface process.

--- bin/start.bat
+++ bin/start.bat
@@ -1,23 +1,35 @@
 @ECHO OFF
+SETLOCAL EnableDelayedExpansion
 
-setlocal
-for %%P in (%PATHEXT%) do (
-    if exist "%%~$PATH:node.exe" (
-      start "server" %%~$PATH:node.exe ./browser/server.js --apikey %RPC_API_KEY% --serverport %SERVER_PORT% --daemon
-      GOTO next
-    )
-  )
+REM Detect no API key
+IF [%RPC_API_KEY%]==[] (
+  ECHO [WARN] RPC_API_KEY not defined. Overwrite it with your custom key to keep track of your tiles
+  ECHO
 )
 
-:next
+REM Default START_MINER
+if [%START_MINER%]==[] (
+  set START_MINER=yes
+)
 
-./bin/decentraland-node \
-  --fast \
-  --loglevel=info \
-  --port=2301 \
-  --httpport=8301 \
-  --contentport=9301 \
-  --prefix="data" \
-  --n=testnet \
-  --apikey=%RPC_API_KEY% \
+REM Default SERVER_PORT
+if [%SERVER_PORT%]==[] (
+  set SERVER_PORT=5000
+)
+
+REM Start web interface
+ECHO Starting Decentraland web dashboard!. The node will start shortly
+START /B node browser\server.js --apikey %RPC_API_KEY% --serverport %SERVER_PORT%
+
+REM Start node
+ECHO Starting node
+.\bin\decentraland-node ^
+  --fast ^
+  --loglevel=info ^
+  --port=2301 ^
+  --httpport=8301 ^
+  --contentport=9301 ^
+  --prefix="data" ^
+  --n=testnet ^
+  --apikey=%RPC_API_KEY% ^
   --startminer=%START_MINER%
